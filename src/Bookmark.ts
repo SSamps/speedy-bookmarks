ad94aed02b1c46ab47c3cee1e2b7c0df7a654701
Simon <simonjsampson@gmail.com>
2022-02-12 21:30:15 +0000

    Added two methods of displaying a bookmark icon in the itemTree

--- src/Bookmark.ts
+++ src/Bookmark.ts
@@ -1,17 +1,20 @@
+import * as path from 'path';
 import * as vscode from 'vscode';
 
 export class Bookmark extends vscode.TreeItem {
-    path: string;
+    bookmarkPath: string;
     lineStart: number;
     lineEnd: number;
     highlightType: string;
     contextValue: string = 'bookmark';
     command: vscode.Command = { command: 'speedy-bookmarks.goToBookmark', title: 'GoTo', arguments: [this] };
     collapsibleState: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None;
+    // iconPath = path.join(__filename, '..', '..', 'resources', 'icons', 'bookmark.svg');
+    iconPath = new vscode.ThemeIcon('bookmark', new vscode.ThemeColor('#e35f'));
 
-    constructor(label: string, path: string, lineStart: number, lineEnd: number, highlightType: string) {
+    constructor(label: string, bookmarkPath: string, lineStart: number, lineEnd: number, highlightType: string) {
         super(label);
-        this.path = path;
+        this.bookmarkPath = bookmarkPath;
         this.lineStart = lineStart;
         this.lineEnd = lineEnd;
         this.highlightType = highlightType;
